c376de08195355a371293d84e48d22d42193288d
Justin <justinanderson101@gmail.com>
2021-04-01 04:54:17 +0000

    safe from SQL injections

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,15 +7,18 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const arguments = process.argv.slice(2)
+
 pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`, [`%${arguments[0]}%`, arguments[1]])
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`)
+    })
   })
-}).catch(err => console.error('query error', err.stack));
+  .catch(err => console.log('query error ', err.stack));
